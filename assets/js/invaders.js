23590a0a71a64745d587080ae90b3cc261a36b73
SebastianBlade <blade254353074@hotmail.com>
2015-12-15 11:51:19 +0800

    Update

--- assets/js/invaders.js
+++ assets/js/invaders.js
@@ -45,10 +45,29 @@ function setupInvaders(invadersImage) {
 
     InvaderController.prototype = {
       start: function (stage) {
+        var runTime = + new Date();
+        var self = this;
+        var container;
+        var canvas;
 
+        self._gameover = false;
+        window.invader_container = container = self.container = new createjs.Container();
+        canvas = stage.canvas;
+        self._stageWidth = canvas.width;
+        self._stageHeight = canvas.height;
+        stage.addChild(container);
+        createjs.Ticker.addEventListener('tick', function (event) {
+          if (self._gameover) return;
+          // 操作敌机逻辑
+          self.tick();
+          stage.update(event);
+        });
       },
       tick: function () {
         var self = this;
+        if (self.setWave()) {
+          self.handleWave();
+        }
         // self._numInvader && self.handleFramerate();
       },
       setWave: function () {
@@ -60,7 +79,7 @@ function setupInvaders(invadersImage) {
         var now = +new Date();
         if (curWave) {
           //当前一轮还未播放完毕
-          if (curWave.compCount) return;
+          if (curWave.compCount) return true;
           // true: 立即,开始下一轮;
           // false: 清屏后,开始下一轮;
           // time: 当前播放完后指定时间后,开始下一轮
@@ -74,6 +93,55 @@ function setupInvaders(invadersImage) {
             nextStart = ~~nextStart;
           }
         }
+        waveMap = self._waveMap.shift();
+        //当没有下一轮,并且页面上也没有任何敌机的时候,游戏结束
+        if (!waveMap && !self._numInvader) {
+          self.gameover();
+					return;
+        }
+        invaders = waveMap.invaders;
+        self._curWave = {
+          startTime: now,
+					nextWave: waveMap.nextWave,
+					invaders: invaders,
+					compCount: invaders.length
+        };
+      },
+			gameover: function(){
+				console.log( (+new Date() - runTime)/1000 );
+				this._gameover = true;
+			},
+      handleWave: function() {
+        var self = this;
+        var curWave = self._curWave;
+				//当前没东西 || 当前一轮已完成,等待下一轮的开启
+        if (!(curWave && curWave.compCount)) return;
+        var curDate = + new Date();
+        var invaders = curWave.invaders;
+        invaders.forEach(function (ivdInfo) {
+          // 生成敌机
+        });
+      },
+      makeInvader: function(invader) {
+        var self = this;
+        var id;
+        // var container = self.container;
+        var name = invader.name;
+        var sheetMap = self._sheetMap[name];
+        var invaderSprite = sheetMap.create();
+
+        invaderSprite._data = {
+          isHit: 0,
+          position: invader.position
+        };
+        // self.setAnimate(invaderSprite, sheetMap);
+        id = invaderSprite.id = 'ivd_' + invaderSprite.id;
+        invaderSprite.name = name;
+        self.collideMap[id] = invaderSprite;
+        // 添加事件
+        invaderSprite.on('hit', self.eventHit, self);
+        self._numInvader++;
+        sheetMap = invaderSprite = null;
       }
     };
   }());
